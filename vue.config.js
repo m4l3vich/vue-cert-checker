73d1cec0564d7882b79747cf8e4ce90c4022d65e
Evgeny Malevich <m4l3vich@mail.ru>
2021-12-21 12:25:19 +0800

    WIP: PWA changes

--- vue.config.js
+++ vue.config.js
@@ -9,5 +9,13 @@ module.exports = {
         { test: /\.wasm.bin$/i, use: [{ loader: 'file-loader' }] }
       ]
     }
+  },
+
+  pwa: {
+    name: 'Сканер сертификатов',
+    themeColor: '#000000',
+    msTileColor: '#0085FF',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black'
   }
 }
