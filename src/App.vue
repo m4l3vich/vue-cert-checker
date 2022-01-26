15166371c2ce1d4bc6ef2b7d186ae2f0422aa75b
M4L3VICH <m4l3vich@mail.ru>
2022-01-26 16:17:40 +0800

    Fix custom vh variable
    Now the value is being re-calculated on resize

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,16 @@
 
 <script>
 export default {
-  beforeCreate () {
-    const vh = window.innerHeight * 0.01
-    document.documentElement.style.setProperty('--vh', `${vh}px`)
+  created () {
+    this.calcHeight()
+    window.addEventListener('resize', () => this.calcHeight())
+  },
+
+  methods: {
+    calcHeight () {
+      const vh = window.innerHeight * 0.01
+      document.documentElement.style.setProperty('--vh', `${vh}px`)
+    }
   }
 }
 </script>
